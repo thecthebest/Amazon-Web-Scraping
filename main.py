ae4adfddda0f38f6c575a886e046ba9108b6076d
thecthebest <asyed5839@gmail.com>
2022-06-14 10:38:02 +0100

    Library for working with datetime

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ import requests
 from bs4 import BeautifulSoup
 # library for working with csv files
 import csv
+# library for working with datetime
+import datetime as dt
 
 # url of the website
 URL = 'https://www.amazon.co.uk/Levis-Classic-Twill-Baseball-Black/dp/B00FOI1JJ4/ref=sr_1_7?crid=1T3KFV41JZN4T&keywords=cap&qid=1655196251&sprefix=cap%2Caps%2C64&sr=8-7'
